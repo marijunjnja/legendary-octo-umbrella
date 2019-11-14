8bf0bbf8a7f10acdfa03268d2091c1694f06b6d9
Bannon Tanner <bannon.nicholas.tanner@ibm.com>
2019-11-14 14:27:52 -0600

    new api with https and more info for the ISS

--- src/components/Distance.js
+++ src/components/Distance.js
@@ -4,14 +4,28 @@ import * as geolib from 'geolib'
 const Distance = ({ myPosition }) => {
   const [distance, setDistance] = React.useState(null)
   const [issPosition, setIssPosition] = React.useState(null)
+  const [issInfo, setIssInfo] = React.useState(null)
 
   React.useEffect(() => {
     // wrap fetch call inside an async function
     const getData = async () => {
-      const url = 'http://api.open-notify.org/iss-now.json'
+      const url = 'https://api.wheretheiss.at/v1/satellites/25544?units=miles'
       const res = await fetch(url)
       const data = await res.json()
-      const position = data.iss_position
+      const info = {
+        longitude: data.longitude,
+        latitude: data.latitude,
+        altitude: data.altitude,
+        velocity: data.velocity,
+        visibility: data.visibility,
+        solar_lat: data.solar_lat,
+        solar_lon: data.solar_lon,
+      }
+      setIssInfo(info)
+      const position = {
+        longitude: info.longitude,
+        latitude: info.latitude,
+      }
       const distanceFromIss = geolib.getDistance(myPosition, position)
       const distanceFromIssMiles = geolib.convertDistance(distanceFromIss, 'mi')
       // instead of returning data, set state
@@ -21,7 +35,7 @@ const Distance = ({ myPosition }) => {
     // call it inside the useEffect
     const timer = setInterval(() => {
       getData()
-    }, 1000)
+    }, 5000)
 
     return () => clearTimeout(timer)
     // pass our dependencies. when they change, the effect is ran again
@@ -34,8 +48,14 @@ const Distance = ({ myPosition }) => {
         <h3>ISS Position</h3>
         <p>Latitude: {issPosition.latitude}</p>
         <p>Longitude: {issPosition.longitude}</p>
-        <h3>Distance</h3>
+        <h3>Distance from your Location</h3>
         <p>{distance} miles</p>
+        <h3>Other Info</h3>
+        <p>Altitude: {issInfo.altitude} miles</p>
+        <p>Velocity: {issInfo.velocity} mph</p>
+        <p>Visibility: {issInfo.visibility}</p>
+        <p>Solar Latitude: {issInfo.solar_lat}</p>
+        <p>Solar Longitude: {issInfo.solar_lon}</p>
       </div>
     )
   }
